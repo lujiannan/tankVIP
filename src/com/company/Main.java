0d65ce56c53365b89f1e5409eb04f294ef7ac4e0
Jonas Lu <nannan001204@gmail.com>
2023-03-06 15:34:38 +0800

    new出5个敌方坦克

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,26 +5,16 @@ import java.awt.*;
 public class Main {
 
     public static void main(String[] args) throws InterruptedException {
-//        Frame f =  new Frame();
-//
-//        // 设置窗口属性
-//        f.setSize(800,600);
-//        f.setResizable(false);
-//        f.setTitle("Tank War");
-//        f.setVisible(true);
-//
-//        // 添加 点击×时，关闭窗口
-//        f.addWindowListener(new WindowAdapter() {
-//            @Override
-//            public void windowClosing(WindowEvent e) {
-//                super.windowClosing(e);
-//                System.exit(0);
-//            }
-//        });
-        Frame f = new TankFrame();
+        TankFrame tf = new TankFrame();
+
+        // 初始化敌方坦克
+        for (int i = 0; i < 5; i++){
+            tf.tanks.add(new Tank(50 + i * 80, 120, Dir.DOWN, tf));
+        }
+
         while (true) {
             Thread.sleep(50);
-            f.repaint();
+            tf.repaint();
         }
     }
 }
--- src/com/company/TankFrame.java
+++ src/com/company/TankFrame.java
@@ -10,8 +10,9 @@ import java.util.List;
 
 public class TankFrame extends Frame {
 
-    Tank myTank = new Tank(200, 200, Dir.DOWN, this);
+    Tank myTank = new Tank(200, 400, Dir.UP, this);
     List<Bullet> bullets = new ArrayList<>();
+    List<Tank> tanks = new ArrayList<>();
     Bullet b = new Bullet(200,200, Dir.DOWN, this);
     static final int GAME_WIDTH = 800, GAME_HEIGHT = 600;
 
@@ -55,12 +56,17 @@ public class TankFrame extends Frame {
     public void paint(Graphics g) {
         super.paint(g);
 
+        Color c = g.getColor();
         g.setColor(Color.WHITE);
         g.drawString("bullets: " + bullets.size(), 20, 60);
+        g.setColor(c);
 
         myTank.paint(g);
         for (int i = 0; i < bullets.size(); i++){
             bullets.get(i).paint(g);
+        }
+        for (int i = 0; i < tanks.size(); i++){
+            tanks.get(i).paint(g);
         }
     }
 
