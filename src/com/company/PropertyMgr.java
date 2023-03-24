12d13755ca64e28b7db549574d56658bf2190af6
Jonas Lu <nannan001204@gmail.com>
2023-03-24 20:44:36 +0800

    加入配置文件，让程序更灵活

---
+++ src/com/company/PropertyMgr.java
@@ -0,0 +1,25 @@
+/**
+ * this class is used to load the config text file into the program for other classes to get
+ */
+
+package com.company;
+
+import java.io.IOException;
+import java.util.Properties;
+
+public class PropertyMgr {
+    static Properties props = new Properties();
+
+    static {
+        try {
+            props.load(PropertyMgr.class.getClassLoader().getResourceAsStream("config"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static Object get(String key) {
+        if (props == null) return null;
+        return props.get(key);
+    }
+}
